96f1abbac41fd971e234e3801ace4c20532408ce
Valentine Storozhuk <stolentine@gmail.com>
2021-04-16 15:20:01 +0300

    PS-2021.1 <lugbe@DESKTOP-U2CS8N0 Create markdown.xml, PHP Class.php, MyPhp.xml, Provider.php, Enum.php, Job.php, Default.xml, find.xml, keymap.xml, stolentine IntelliJ Light.icls, Default.xml, PHP.xml, JavaScript.xml, lightEdit.xml, PHP Setter Method.php, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_Default.icls, web-types-registry.xml, HandlerClass.php, Dto.php, Default copy123.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, php.xml, remote-servers.xml, colors.scheme.xml, notifications.xml, PHP Trait.php, databaseDrivers.xml, templates.xml, Vue.xml, stringManipulation.xml, customization.xml, Windows copy.xml, sshConfigs.xml, stolentine Darcula.icls, laf.xml, parameter.hints.xml, PHP Fluent Setter Method.php, web-browsers.xml, wsl.distributions.xml, vcs.xml, PHP Interface.php, evil_topcargo.xml, databaseSettings.xml, grazie_global.xml, usageView.xml

---
+++ fileTemplates/HandlerClass.php
@@ -0,0 +1,19 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+
+class ${NAME}
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function handle()
+    {
+        
+    }
+}
