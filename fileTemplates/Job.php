4839eb51f36e3a7eee13604538049735f5427df2
Валентин Сторожук <vstorozhuk@evilcoders.me>
2021-06-26 10:44:49 +0300

    init

---
+++ fileTemplates/Job.php
@@ -0,0 +1,28 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ${NAME} implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle()
+    {
+        //
+    }
+}
